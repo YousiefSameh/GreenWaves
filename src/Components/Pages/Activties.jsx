57b591f8222cb0dbbbc70268d57b27baaee27bed
Ibrahim Mahmoud <hatab2475@gmail.com>
2024-10-09 15:15:06 +0300

    Add Activties Page

--- src/Components/Pages/Activties.jsx
+++ src/Components/Pages/Activties.jsx
@@ -3,7 +3,66 @@ import Header from "../UI/Header"
 const Activties = () => {
   return (
     <>
+     {/* Start Header */}
      <Header />
+     {/* End Header */}
+     {/* Start Activties */}
+     <main>
+          <section>
+               <div className="act container bg-white shadow-2xl max-w-6xl mx-auto items-center mt-[100px] py-40 rounded-2xl">
+                    <div className="flex">
+                         <div>
+                              <h1 className="-my-20 mr-10 text-3xl font-bold">حملة تنظيف الشواطئ</h1>
+                              <p className="my-24 mr-10">هذه الفعالية تدعو المستخدمين للمشاركة في تنظيف الشواطئ المحلية من النفايات والمخلفات 
+البلاستيكية التي تؤثر سلبًا على البيئة البحرية. الهدف هو تشجيع الناس على العناية بالشواطئ 
+والحفاظ على النظام البيئي الساحلي. يُطلب من المشاركين التوجه إلى الشواطئ القريبة منهم 
+والتقاط النفايات العشوائية التي تضر بالكائنات البحرية والبيئة. بعد الانتهاء، يقوم كل مشارك 
+بتصوير الشاطئ قبل وبعد التنظيف، ثم يرفع الصور على صفحة الفعالية مع تعليق يتضمن 
+تجربته، والصعوبات التي واجهها، وعدد الأكياس التي تم جمعها.</p>
+                              <button className=" bg-primary pl-20 pr-20 py-5 rounded-2xl mr-10 hover:bg-secondary transition-all"><p className="text-xl font-bold text-white">انضم الأن</p></button>
+                         </div>
+                         <div className="img-act bg-slate-400 w-11/12 h-80 -my-40  rounded-l-2xl"></div>
+                    </div>
+               </div>
+          </section>
+
+          <section>
+               <div className="act container bg-white shadow-2xl max-w-6xl mx-auto items-center mt-[100px] py-40 rounded-2xl">
+                    <div className="flex">
+                         <div>
+                              <h1 className="-my-20 mr-10 text-3xl font-bold">حملة تنظيف الشواطئ</h1>
+                              <p className="my-24 mr-10">هذه الفعالية تدعو المستخدمين للمشاركة في تنظيف الشواطئ المحلية من النفايات والمخلفات 
+البلاستيكية التي تؤثر سلبًا على البيئة البحرية. الهدف هو تشجيع الناس على العناية بالشواطئ 
+والحفاظ على النظام البيئي الساحلي. يُطلب من المشاركين التوجه إلى الشواطئ القريبة منهم 
+والتقاط النفايات العشوائية التي تضر بالكائنات البحرية والبيئة. بعد الانتهاء، يقوم كل مشارك 
+بتصوير الشاطئ قبل وبعد التنظيف، ثم يرفع الصور على صفحة الفعالية مع تعليق يتضمن 
+تجربته، والصعوبات التي واجهها، وعدد الأكياس التي تم جمعها.</p>
+                              <button className=" bg-primary pl-20 pr-20 py-5 rounded-2xl mr-10 hover:bg-secondary transition-all"><p className="text-xl font-bold text-white">انضم الأن</p></button>
+                         </div>
+                         <div className="img-act bg-slate-400 w-11/12 h-80 -my-40  rounded-l-2xl"></div>
+                    </div>
+               </div>
+          </section>
+
+          <section>
+               <div className="act container bg-white shadow-2xl max-w-6xl mx-auto items-center mt-[100px] py-40 rounded-2xl">
+                    <div className="flex">
+                         <div>
+                              <h1 className="-my-20 mr-10 text-3xl font-bold">حملة تنظيف الشواطئ</h1>
+                              <p className="my-24 mr-10">هذه الفعالية تدعو المستخدمين للمشاركة في تنظيف الشواطئ المحلية من النفايات والمخلفات 
+البلاستيكية التي تؤثر سلبًا على البيئة البحرية. الهدف هو تشجيع الناس على العناية بالشواطئ 
+والحفاظ على النظام البيئي الساحلي. يُطلب من المشاركين التوجه إلى الشواطئ القريبة منهم 
+والتقاط النفايات العشوائية التي تضر بالكائنات البحرية والبيئة. بعد الانتهاء، يقوم كل مشارك 
+بتصوير الشاطئ قبل وبعد التنظيف، ثم يرفع الصور على صفحة الفعالية مع تعليق يتضمن 
+تجربته، والصعوبات التي واجهها، وعدد الأكياس التي تم جمعها.</p>
+                              <button className=" bg-primary pl-20 pr-20 py-5 rounded-2xl mr-10 hover:bg-secondary transition-all"><p className="text-xl font-bold text-white">انضم الأن</p></button>
+                         </div>
+                         <div className="img-act bg-slate-400 w-11/12 h-80 -my-40  rounded-l-2xl"></div>
+                    </div>
+               </div>
+          </section>
+     </main>
+     {/* End Activties */}
     </>
   )
 }
